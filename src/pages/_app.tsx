301dda8d1c0c40be38af0f2c2b5c51ed9a852fdd
NicolasNasr <dev01@softrasys.com>
2023-05-17 10:41:02 +0300

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <title>Opal Event Management - Coming Soon</title>
+      <link rel="shortcut icon" href="/opal-logo-round.jpg" type="image/x-icon" />
+    </Head>
+    <div>
+      <Component {...pageProps} />
+    </div>
+  </>
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        'dark-purple':'#14152c',
+        'light-purple':'#161246',
+        'primary-white':'#fefefb',
+        'primary-pink':'#e91b77',
+        'primary-blue':'#6abae0',
+      }
     },
   },
   plugins: [],
