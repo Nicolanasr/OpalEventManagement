6ab6b0228d1abccaa3d7b2226cd3448dbc179f1b
NicolasNasr <dev01@softrasys.com>
2023-05-17 11:19:41 +0300

    meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,18 +13,18 @@ export default function App({ Component, pageProps }: AppProps) {
     
       <meta property="og:title" content="Event Planning Services | Opal Event Management" />
       <meta property="og:description" content="Plan your dream event with ease. Our expert event planners will handle every detail, from start to finish, creating unforgettable experiences. Stress-free, personalized, and flawless event coordination awaits. Contact us today!" />
-      <meta property="og:image" content="/opal-logo-nobg.png" />
+      <meta property="og:image" content="/opal-logo.jpg" />
       <meta property="og:url" content="https://opaleventmanagement.com" />
       
       <meta name="twitter:title" content="Event Planning Services | Opal Event Management" />
       <meta name="twitter:description" content="Plan your dream event with ease. Our expert event planners will handle every detail, from start to finish, creating unforgettable experiences. Stress-free, personalized, and flawless event coordination awaits. Contact us today!" />
-      <meta name="twitter:image" content="/opal-logo-nobg.png" />
+      <meta name="twitter:image" content="/opal-logo.jpg" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="https://opaleventmanagement.com" />
       
       <meta property="pinterest:title" content="Event Planning Services | Opal Event Management" />
       <meta property="pinterest:description" content="Plan your dream event with ease. Our expert event planners will handle every detail, from start to finish, creating unforgettable experiences. Stress-free, personalized, and flawless event coordination awaits. Contact us today!" />
-      <meta property="pinterest:image" content="/opal-logo-nobg.png" />
+      <meta property="pinterest:image" content="/opal-logo.jpg" />
     </Head>
     <div>
       <Component {...pageProps} />
